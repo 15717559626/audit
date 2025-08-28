aa852f1d490827f6ef443a8c731df4c1e689e50e
李松桦 <lishua@szlanyou.com>
2025-08-28 10:50:20 +0800

    refactor: 删除无用代码并简化 HelloController

--- src/main/java/com/snow/audit/controller/HelloController.java
+++ src/main/java/com/snow/audit/controller/HelloController.java
@@ -1,14 +1,9 @@
 package com.snow.audit.controller;
 
 import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
-import com.baomidou.mybatisplus.extension.exceptions.ApiException;
-import com.snow.audit.entity.param.WxLoginPram;
-import com.snow.audit.service.HelloService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,23 +18,13 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     @Autowired
-    private HelloService helloService;
-
-    @Autowired
     private WxMaService wxMaService;
 
 
     @ApiOperation(value = "测试接口")
     @GetMapping("/hello")
     public String sayHello(@RequestParam("name") String name){
-        return helloService.hello(name);
-
-    }
-
-    @ApiOperation(value = "工厂接口")
-    @GetMapping("/factory")
-    public String factory(@RequestParam("type") String type){
-        return helloService.eatFood(type);
+        return "hello";
 
     }
 
